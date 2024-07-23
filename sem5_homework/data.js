976d1e3f815cf9ac767045e447f40bcf02921660
ilya.devel <ilya.devel@yandex.ru>
2024-07-23 12:22:10 +0500

    add some fix

--- sem5_homework/data.js
+++ sem5_homework/data.js
@@ -1,3 +1,5 @@
 export const characters = await fetch('https://rickandmortyapi.com/api/character')
-    .then(response => {return response.json()})
-    .catch(error => {return error})
+    .then(response => response.json())
+    .catch(error => {
+        return {"error": error}
+    })
--- sem5_homework/script.js
+++ sem5_homework/script.js
@@ -2,26 +2,28 @@ import {characters} from './data.js'
 
 const content = document.querySelector('.content')
 
-characters.results.forEach(character => {
-    console.log(character)
+if (!characters.error) {
+    characters.results.forEach(character => {
+        const characterBox = document.createElement('div')
+        characterBox.classList.add('character-box')
 
-    const characterBox = document.createElement('div')
-    characterBox.classList.add('character-box')
+        const characterImage = document.createElement('img')
+        characterImage.classList.add('image')
+        characterImage.src = character.image
+        characterImage.alt = character.name
 
-    const characterImage = document.createElement('img')
-    characterImage.classList.add('image')
-    characterImage.src = character.image
-    characterImage.alt = character.name
+        const characterName = document.createElement('p')
+        characterName.classList.add('title')
+        characterName.textContent = character.name
 
-    const characterName = document.createElement('p')
-    characterName.classList.add('title')
-    characterName.textContent = character.name
+        const characterLocation = document.createElement('p')
+        characterLocation.textContent = `Родился в: ${character.location.name}`
 
-    const characterLocation = document.createElement('p')
-    characterLocation.textContent = `Родился в: ${character.location.name}`
-
-    characterBox.insertAdjacentElement('beforeend', characterImage)
-    characterBox.insertAdjacentElement('beforeend', characterName)
-    characterBox.insertAdjacentElement('beforeend', characterLocation)
-    content.insertAdjacentElement('beforeend', characterBox)
-})
+        characterBox.insertAdjacentElement('beforeend', characterImage)
+        characterBox.insertAdjacentElement('beforeend', characterName)
+        characterBox.insertAdjacentElement('beforeend', characterLocation)
+        content.insertAdjacentElement('beforeend', characterBox)
+    })
+} else {
+    alert(characters.error.message)
+}
